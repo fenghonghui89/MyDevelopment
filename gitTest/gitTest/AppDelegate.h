e68ddd82fff367570f9f6ddb32c8a99248324de6
hanyfeng <hanyfeng89@163.com>
2015-06-09 18:08:37 +0800

    test

---
+++ gitTest/gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by hanyfeng on 15/6/9.
+//  Copyright (c) 2015年 MD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
