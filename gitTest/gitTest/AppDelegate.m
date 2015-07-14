1418712faea6c81d5e630aa60102a0be33c4e18f
hanyfeng <hanyfeng89@163.com>
2015-07-14 15:59:18 +0800

    t1

--- gitTest/gitTest/AppDelegate.m
+++ gitTest/gitTest/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    int 1;
+    int i = 1;
     return YES;
 }
 
