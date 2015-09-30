63ccc69b8df0d1fea69b59d9a5d671873b64cf98
hanyfeng <hanyfeng@macbooktekiMacBook-Pro.local>
2015-09-30 14:08:16 +0800

    af

---
+++ 网络1/AFNetworkTest/AFNetworkTest/AFNetworkManager.h
@@ -0,0 +1,13 @@
+//
+//  AFNetworkManager.h
+//  AFNetworkTest
+//
+//  Created by hanyfeng on 15/9/28.
+//  Copyright © 2015年 hanyfeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AFNetworkManager : NSObject
+
+@end
---
+++ 网络1/AFNetworkTest/AFNetworkTest/AFNetworkManager.m
@@ -0,0 +1,13 @@
+//
+//  AFNetworkManager.m
+//  AFNetworkTest
+//
+//  Created by hanyfeng on 15/9/28.
+//  Copyright © 2015年 hanyfeng. All rights reserved.
+//
+
+#import "AFNetworkManager.h"
+
+@implementation AFNetworkManager
+
+@end
