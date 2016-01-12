99fd8b88bda38e789e65765c1ad18b5d387b64d7
冯鸿辉 <hanyfeng@fenghonghuideMac-mini.local>
2016-01-12 17:40:23 +0800

    *Masonry

---
+++ 功能实现/Masonry/MasonryTest/MasonryTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MasonryTest
+//
+//  Created by 冯鸿辉 on 16/1/12.
+//  Copyright © 2016年 DGC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 功能实现/Masonry/MasonryTest/MasonryTest/UIView+Masonry_Hany.h
@@ -0,0 +1,14 @@
+//
+//  UIView+Masonry_Hany.h
+//  MasonryTest
+//
+//  Created by 冯鸿辉 on 16/1/12.
+//  Copyright © 2016年 DGC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Masonry_Hany)
+- (void) distributeSpacingHorizontallyWith:(NSArray*)views;
+- (void) distributeSpacingVerticallyWith:(NSArray*)views;
+@end
