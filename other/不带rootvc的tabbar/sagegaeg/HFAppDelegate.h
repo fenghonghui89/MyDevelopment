748be186f7b1448e9cb53002b462ddacaca90295
冯鸿辉 <hanyfeng@fenghonghuideMac-mini.local>
2016-01-13 18:29:30 +0800

    *不带rootvc的tabbar

---
+++ other/不带rootvc的tabbar/sagegaeg/HFAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  HFAppDelegate.h
+//  sagegaeg
+//
+//  Created by hanyfeng on 14-5-23.
+//  Copyright (c) 2014年 hanyfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HFAppDelegate : UIResponder <UIApplicationDelegate>
+@property (weak, nonatomic) IBOutlet UITabBarController *tabs;
+
+@property (weak, nonatomic) IBOutlet UIWindow *window;
+
+
+
+@end
