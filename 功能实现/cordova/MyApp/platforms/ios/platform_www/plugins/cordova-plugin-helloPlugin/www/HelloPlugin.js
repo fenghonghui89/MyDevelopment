3ea3cd4aec648d15dd6d041684af662cf207dd6a
冯鸿辉 <hanyfeng@fenghonghuideMac-mini.local>
2016-02-24 16:55:53 +0800

    *remove

--- 功能实现/cordova/MyApp/platforms/ios/platform_www/plugins/cordova-plugin-helloPlugin/www/HelloPlugin.js
+++
@@ -1,8 +0,0 @@
-cordova.define("cordova-plugin-helloPlugin.HelloPlugin", function(require, exports, module) {
-var exec = require('cordova/exec');
-
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "HelloPlugin", "coolMethod", [arg0]);
-};
-
-});
--- 功能实现/cordova/MyApp/platforms/ios/www/plugins/cordova-plugin-echoPlugin/www/Echo.js
+++
@@ -1,9 +0,0 @@
-cordova.define("cordova-plugin-echoPlugin.Echo", function(require, exports, module) {
-
-
-window.echo = function(str, callback) {
-  cordova.exec(callback, function(err) {
-               callback('Nothing to echo.');
-               }, "Echo", "echo", [str]);
-};
-});
