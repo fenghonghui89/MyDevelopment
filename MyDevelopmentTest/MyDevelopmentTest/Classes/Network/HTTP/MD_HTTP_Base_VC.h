1d9ea9bf0d2839a0ea65a3b5d2b9988e33d3b493
冯鸿辉 <hanyfeng@fenghonghuideMac-mini.local>
2016-03-22 18:30:54 +0800

    *http base

---
+++ MyDevelopmentTest/MyDevelopmentTest/Classes/Network/HTTP/MD_HTTP_Base_VC.h
@@ -0,0 +1,13 @@
+//
+//  MD_HTTP_Base_VC.h
+//  MyDevelopmentTest
+//
+//  Created by 冯鸿辉 on 16/3/22.
+//  Copyright © 2016年 hanyfeng. All rights reserved.
+//
+
+#import "MDBaseViewController.h"
+
+@interface MD_HTTP_Base_VC : MDBaseViewController
+
+@end
