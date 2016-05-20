20b59a7c6a2b60ce71a97f5df202eaf084c0e66b
冯鸿辉 <hanyfeng@fenghonghuideMac-mini.local>
2016-05-20 18:24:09 +0800

    *code adjust

--- MyDevelopmentTest/MyDevelopmentTest/Classes/Network/WebKit/MD_WebKit_VC.m
+++ MyDevelopmentTest/MyDevelopmentTest/Classes/Network/WebKit/MD_WebKit_VC.m
@@ -7,10 +7,10 @@
 //
 
 #import "MD_WebKit_VC.h"
-@import WebKit;
+//@import WebKit;
 
-@interface MD_WebKit_VC ()<WKNavigationDelegate>
-@property(nonatomic,strong)WKWebView *webView;
+@interface MD_WebKit_VC ()
+//@property(nonatomic,strong)WKWebView *webView;
 @end
 
 @implementation MD_WebKit_VC
@@ -30,29 +30,29 @@
 }
 #pragma mark - < method >
 
--(void)commonInitUI{
-
-  WKWebView *webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 0, viewW, viewH)];
-  webView.navigationDelegate = self;
-  [self.view addSubview:webView];
-  
-  [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.baidu.com"]]];
-}
-#pragma mark - < action >
-#pragma mark - < callback >
--(void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation{
-  NSLog(@"didStartProvisionalNavigation");
-}
-
--(void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation{
-  NSLog(@"didCommitNavigation");
-}
-
--(void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation{
-  NSLog(@"didFinishNavigation");
-}
-
--(void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error{
-  NSLog(@"didFailNavigation");
-}
+//-(void)commonInitUI{
+//
+//  WKWebView *webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 0, viewW, viewH)];
+//  webView.navigationDelegate = self;
+//  [self.view addSubview:webView];
+//  
+//  [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.baidu.com"]]];
+//}
+//#pragma mark - < action >
+//#pragma mark - < callback >
+//-(void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation{
+//  NSLog(@"didStartProvisionalNavigation");
+//}
+//
+//-(void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation{
+//  NSLog(@"didCommitNavigation");
+//}
+//
+//-(void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation{
+//  NSLog(@"didFinishNavigation");
+//}
+//
+//-(void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error{
+//  NSLog(@"didFailNavigation");
+//}
 @end
