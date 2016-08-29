fc0f7a17a1a168c91c1aafeee092adbb0011527d
冯鸿辉 <hanyfeng@fenghonghuideMac-mini.local>
2016-08-29 14:38:08 +0800

    *css

---
+++ HanyHTMLLanguageDemo/HTML/php/demo_see.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: text/event-stream');//把报头 "Content-Type" 设置为 "text/event-stream
+header('Cache-Control: no-cache');//规定不对页面进行缓存
+
+//输出发送日期（始终以 "data: " 开头）
+$time = date('r');
+echo "data: The server time is: {$time}\n\n";
+
+flush();//向网页刷新输出数据
