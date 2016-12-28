ae4f462df0f41b49622dc2442ee3594f13b13849
冯鸿辉 <298090563@qq.com>
2016-12-28 18:42:23 +0800

    * ajax

---
+++ HanyWebDevelopWorkspace/src/DataSource/MyServer3.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hanyfeng
+ * Date: 2016/12/28
+ * Time: 17:24
+ */
+
+/*
+当请求从 JavaScript 发送到 PHP 页面时，发生：
+PHP 创建 "cd_catalog.xml" 文件的 XML DOM 对象
+循环所有 "artist" 元素 (nodetypes = 1)，查找与 JavaScript 所传数据向匹配的名字
+找到 CD 包含的正确 artist
+输出 album 的信息，并发送到 "txtHint" 占位符
+*/
+
+$q=$_GET["q"];
+
+$xmlDoc = new DOMDocument();
+$xmlDoc->load("cd_catalog.xml");
+
+$x=$xmlDoc->getElementsByTagName('ARTIST');
+
+for ($i=0; $i<=$x->length-1; $i++)
+{
+//Process only element nodes
+    if ($x->item($i)->nodeType==1)
+    {
+        if ($x->item($i)->childNodes->item(0)->nodeValue == $q)
+        {
+            $y=($x->item($i)->parentNode);
+        }
+    }
+}
+
+$cd=($y->childNodes);
+
+for ($i=0;$i<$cd->length;$i++)
+{
+//Process only element nodes
+    if ($cd->item($i)->nodeType==1)
+    {
+        echo($cd->item($i)->nodeName);
+        echo(": ");
+        echo($cd->item($i)->childNodes->item(0)->nodeValue);
+        echo("<br />");
+    }
+}
---
+++ HanyWebDevelopWorkspace/src/Tool/Tool_JS.js
@@ -0,0 +1,30 @@
+/**
+ * Created by hanyfeng on 2016/12/28.
+ */
+
+function tool_GetXmlHttpObject(){
+
+    var xmlHttp = null
+
+    try
+    {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        // 或者用 if (window.XMLHttpRequest) 检查是否支持 XMLHttpRequest对象
+        xmlHttp = new XMLHttpRequest()
+    }
+    catch (e)
+    {
+        try
+        {
+            // IE6
+            xmlHttp = new ActiveXObject('Msxml2.XMLHTTP')
+        }
+        catch (e)
+        {
+            // IE6 IE5
+            xmlHttp = new ActiveXObject('Microsoft.XMLHTTP')
+        }
+    }
+
+    return xmlHttp
+}
