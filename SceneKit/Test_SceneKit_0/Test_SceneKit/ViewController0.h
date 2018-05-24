1d3a80b3fad92a929e01c46cd7c0bc3baf74db9c
hanyfeng <298090563@qq.com>
2018-05-24 12:50:48 +0800

    *测试项目

--- SceneKit/Test_SceneKit_0/Test_SceneKit/ViewController0.h
+++ SceneKit/Test_SceneKit_0/Test_SceneKit/ViewController0.h
@@ -7,9 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+// Collision bit masks
+typedef NS_OPTIONS(NSUInteger, AAPLBitmask) {
+    AAPLBitmaskCollision        = 1UL << 2,
+    AAPLBitmaskCollectable      = 1UL << 3,
+    AAPLBitmaskEnemy            = 1UL << 4,
+    AAPLBitmaskSuperCollectable = 1UL << 5,
+    AAPLBitmaskWater            = 1UL << 6
+};
 
 @interface ViewController0 : UIViewController
-
+@property (nonatomic, strong)SCNView *gameView;
 
 @end
 
