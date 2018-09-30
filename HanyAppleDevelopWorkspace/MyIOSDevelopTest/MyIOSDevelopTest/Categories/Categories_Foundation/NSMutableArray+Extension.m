9972d0e913417093bb4a183578becd3fe72452bc
hanyfeng <298090563@qq.com>
2018-09-30 16:29:59 +0800

    * runtime注释

--- HanyAppleDevelopWorkspace/MyIOSDevelopTest/MyIOSDevelopTest/Categories/Categories_Foundation/NSMutableArray+Extension.m
+++ HanyAppleDevelopWorkspace/MyIOSDevelopTest/MyIOSDevelopTest/Categories/Categories_Foundation/NSMutableArray+Extension.m
@@ -20,34 +20,49 @@
 
 #if flag == 1
 +(void)load{
-  
-  DRLog(@"load NSMutableArray+Extension..");
-  
-  /*
-   __NSArrayM是NSMutableArray的真正类型
-   */
-  SEL orignalSel = @selector(addObject:);
-  SEL swizzledSel = @selector(md_addObject:);
-  Class thClass = NSClassFromString(@"__NSArrayM");
-  
-  Method orignalMethod = class_getInstanceMethod(thClass, orignalSel);
-  Method swizzledMethod = class_getInstanceMethod(thClass, swizzledSel);
-  
-  BOOL canAdd = class_addMethod(thClass,
-                                orignalSel,
-                                method_getImplementation(swizzledMethod),
-                                method_getTypeEncoding(swizzledMethod));
-  if (canAdd) {
-    NSLog(@"can add..");
-    class_replaceMethod(thClass,
-                        swizzledSel,
-                        method_getImplementation(orignalMethod),
-                        method_getTypeEncoding(orignalMethod));
-  }else{
-    NSLog(@"can not add..");
-    method_exchangeImplementations(orignalMethod, swizzledMethod);
-  }
-  
+    
+    DRLog(@"load NSMutableArray+Extension..");
+    
+    /*
+     __NSArrayM是NSMutableArray的真正类型
+     */
+    SEL orignalSel = @selector(addObject:);
+    SEL swizzledSel = @selector(md_addObject:);
+    Class thClass = NSClassFromString(@"__NSArrayM");
+    
+    Method orignalMethod = class_getInstanceMethod(thClass, orignalSel);
+    IMP orignalIMP = method_getImplementation(orignalMethod);
+    const char *originalType = method_getTypeEncoding(orignalMethod);
+    
+    Method swizzledMethod = class_getInstanceMethod(thClass, swizzledSel);
+    IMP swizzledIMP = method_getImplementation(swizzledMethod);
+    const char *swizzledType = method_getTypeEncoding(swizzledMethod);
+    
+    BOOL canAdd = class_addMethod(thClass,
+                                  orignalSel,
+                                  swizzledIMP,
+                                  swizzledType);
+    
+    if (canAdd) {//如果方法没有存在,我们则先尝试添加被替换的方法的实现
+        NSLog(@"can add..");
+        
+        /*
+         class_replaceMethod本身会尝试调用class_addMethod和method_setImplementation，所以直接调用class_replaceMethod就可以了
+         参数：要操作的class 要操作的方法sel 新的实现 新的typeencoding
+         */
+        class_replaceMethod(thClass,
+                            swizzledSel,
+                            orignalIMP,
+                            originalType);
+        class_replaceMethod(thClass,
+                            orignalSel,
+                            swizzledIMP,
+                            swizzledType);
+    }else{//如果发现方法已经存在，class_addMethod会失败返回
+        NSLog(@"can not add..");
+        method_exchangeImplementations(orignalMethod, swizzledMethod);
+    }
+    
 }
 #elif flag == 2
 
@@ -60,9 +75,9 @@
 
 
 -(void)md_addObject:(id)object{
-
-  if(object != nil){
-    [self md_addObject:object];
-  }
+    
+    if(object != nil){
+        [self md_addObject:object];
+    }
 }
 @end
